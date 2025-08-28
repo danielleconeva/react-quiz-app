5797ee5c424d44450309efd30cb28207c2a9ffa2
danielleconeva <dconewa@gmail.com>
2025-08-28 11:02:01 +0300

    Add quiz logic with shuffled answers, timers, effect cleanup and component reset via keys

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,23 +1,39 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import QUESTIONS from '../questions.js';
+import QuestionTimer from "./QuestionTimer.jsx";
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
-
     const activeQuestionindex = userAnswers.length;
+    const quizIsComplete = activeQuestionindex === QUESTIONS.length;
 
-    function handleSelectAnswer(selectedAnswer) {
+
+    const handleSelectAnswer = useCallback(function handleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevUserAnswers) => {
             return [...prevUserAnswers, selectedAnswer]
         });
+    }, []);
+
+    const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
+
+    if (quizIsComplete) {
+        return <div id="summary">
+            <img src={quizCompleteImg} alt="Trophy Icon"></img>
+            <h2>Quiz Completed!</h2>
+        </div>
     }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionindex].answers];
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer key={activeQuestionindex} timeout={10000} onTimeout={handleSkipAnswer} />
                 <h2>{QUESTIONS[activeQuestionindex].text}</h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionindex].answers.map(answer => (
+                    {shuffledAnswers.map(answer => (
                         <li key={answer} className="answer">
                             <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
                         </li>))}
